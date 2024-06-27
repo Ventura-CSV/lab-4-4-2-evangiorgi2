1387a7cbfb1565546f1e7ffea2c7a5e268fb6e95
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 16:09:36 -0700

    Error

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+    while True:
+        try:
+            number = int(input('Enter a number: '))
+        except ValueError:
+            print('You must enter a number')
+            continue
+        else:
+            print (number)
+            break
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
     return number
 
 
 if __name__ == '__main__':
+    
     main()
